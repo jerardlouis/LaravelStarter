a84a46a90f25614763f71b7785d47a0afe797b8f
Jerard Guevarra <jerardlguevarra@gmail.com>
2024-05-31 12:24:06 -0700

    update index for SEO purposes

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <!-- App by @jerardlouis Jerard Louis Dayanghirang Guevarra jerard.dev -->
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
